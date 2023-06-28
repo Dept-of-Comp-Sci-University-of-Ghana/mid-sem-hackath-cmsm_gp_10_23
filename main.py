392b203770c513e1684ad8d03f095876fb64fd9b
Obeng-Danso Richard <rdanso2003@gmail.com>
2023-06-28 14:49:06 +0000

    Validation function

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ import tweepy
 import warnings
 import pandas as pd
 import spacy
+import pickle
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import CountVectorizer
 from wordcloud import WordCloud
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import (
+    accuracy_score, precision_score, recall_score, f1_score)
 import nltk
 from nltk.tokenize import word_tokenize
 warnings.filterwarnings('ignore')
 
 
 # Access the dataset
-#dataset = pd.read_csv('./Suicide_Detection.csv', nrows=10000)
-
-dataset = pd.read_csv('~/Documents/Suicide_Detection.csv', nrows=10000)
+dataset = pd.read_csv('~/Documents/Suicide_Detection.csv', nrows=1000)
 
 
 # check to print first 5 rows of the dataset, used to ensure it is being accessed
@@ -112,53 +112,169 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 # build logistic regression model
 log_reg = LogisticRegression().fit(X_train, y_train)
+
 #predict on training data
 y_train_pred = log_reg.predict(X_train)
 
-# calculate accuracy on training data
+# Calculate evaluation metrics on training data
 train_accuracy = accuracy_score(y_train, y_train_pred)
-print("Accuracy on training data:", train_accuracy)
+train_precision = precision_score(y_train, y_train_pred)
+train_recall = recall_score(y_train, y_train_pred)
+train_f1_score = f1_score(y_train, y_train_pred)
 
-#predict on testing data
+# Predict on testing data
 y_test_pred = log_reg.predict(X_test)
 
-#calculate accuracy on testing data
+# Calculate evaluation metrics on testing data
 test_accuracy = accuracy_score(y_test, y_test_pred)
-print("Accuracy on testing data:", test_accuracy)
+test_precision = precision_score(y_test, y_test_pred)
+test_recall = recall_score(y_test, y_test_pred)
+test_f1_score = f1_score(y_test, y_test_pred)
+
+# Create a bar chart for evaluation metrics
+metrics = ['Accuracy', 'Precision', 'Recall', 'F1-score']
+train_scores = [train_accuracy, train_precision, train_recall, train_f1_score]
+test_scores = [test_accuracy, test_precision, test_recall, test_f1_score]
+
 
 # confusion matrix
 print(confusion_matrix(y_test, y_test_pred)/len(y_test))
+
+# Plot a bar chart for the metrics
+plt.figure(figsize=(10, 6))
+plt.bar(metrics, train_scores, label='Train')
+plt.bar(metrics, test_scores, label='Test')
+plt.xlabel('Metrics')
+plt.ylabel('Scores')
+plt.title('Evaluation Metrics')
+plt.legend()
+plt.show()
+
+print(f"train_accuracy: {train_accuracy}")
+print(f"train_precision: {train_precision}")
+print(f"train_recall: {train_recall}")
+print(f"train_f1_score: {train_f1_score}")
+print(f"test_accuracy: {test_accuracy}")
+print(f"test_precision: {test_precision}")
+print(f"test_recall: {test_recall}")
+print(f"test_f1_score: {test_f1_score}")
+
+model_filepath = '/Users/user/PycharmProjects/mid-sem-hackath-cmsm_gp_10_23/model.pkl'
+
+# Save the model
+with open(model_filepath, 'wb') as file:
+    pickle.dump(log_reg, file)
+
+# Load the saved model
+with open(model_filepath, 'rb') as file:
+    loaded_model = pickle.load(file)
+
+#Text Classification Function
+def classify_texts(texts):
+    # Preprocess the texts and convert them to strings
+    cleaned_texts = [preprocessing(text)[0] for text in texts]
+
+    # Transform the preprocessed texts using the same CountVectorizer used during training
+    X_texts = vect.transform(cleaned_texts)
+
+    # Make predictions using the loaded model
+    predictions = loaded_model.predict(X_texts)
+
+    # Map the predicted labels back to their original class names
+    label_mapping = {1: 'suicide', 0: 'non-suicide'}
+    predicted_labels = [label_mapping[prediction] for prediction in predictions]
+
+    # Create a DataFrame with the texts and their corresponding predicted labels
+    results = pd.DataFrame({'Text': texts, 'Predicted Label': predicted_labels})
+
+    return results
+
+
+texts = [
+    "I'm feeling really down today. Nothing seems to be going right.",
+    "I had a great day with my friends. Life is beautiful.",
+    "I can't handle the pain anymore. It's too much to bear.",
+    "I'm so happy! Everything is going well for me.",
+    "I don't see any point in living anymore.",
+    "I achieved a major milestone in my career. Feeling grateful!",
+    "I'm overwhelmed by sadness. It's consuming me.",
+    "Life is full of possibilities. Excited for what's to come!",
+    "I can't shake off this feeling of emptiness.",
+    "Spending time with loved ones brings so much joy.",
+    "I'm hopeless. There's no way out of this darkness.",
+    "Every day is a new opportunity for growth and happiness.",
+    "I can't bear the burden anymore. It's too heavy.",
+    "I feel so loved and supported by my friends and family.",
+    "I want to disappear. The world would be better off without me.",
+    "Nature has a way of healing the soul. Enjoying a peaceful walk.",
+    "I'm at the lowest point in my life. There's no way out.",
+    "Grateful for the little things that bring happiness.",
+    "I can't find any reasons to keep going. It's all meaningless.",
+    "Today is a good day. Feeling positive and motivated!",
+    "I'm drowning in despair. There's no escape.",
+    "I have so much to live for. Excited about the future!",
+    "The pain is unbearable. I just want it to end.",
+    "Finding solace in music. It lifts my spirits.",
+    "I'm contemplating ending it all. I can't go on like this.",
+    "Cherishing the moments of laughter and joy with loved ones.",
+    "I feel like a burden to everyone around me. They'd be better off without me.",
+    "The world is a beautiful place. Grateful for its wonders.",
+    "I can't handle the darkness anymore. It's suffocating.",
+    "Celebrating small victories in life. Every step counts.",
+    "I'm consumed by thoughts of self-harm. It's overwhelming.",
+    "Reaching out for support. It's okay to ask for help.",
+    "I'm surrounded by negativity. It's dragging me down.",
+    "Dreaming big and working towards my goals. Anything is possible!",
+    "I'm exhausted. Living feels like an endless battle.",
+    "The love and kindness of others give me strength to carry on.",
+    "I can't see a way out. The pain is all-consuming.",
+    "Gratitude fills my heart. Appreciating the beauty in everyday moments.",
+    "I can't fight the darkness anymore. It's taking over.",
+    "Enjoying the simple pleasures in life. Grateful for what I have.",
+    "I'm at the edge. The void is calling me.",
+    "Surrounding myself with positivity and uplifting energy.",
+    "I'm lost in a sea of despair. There's no way back.",
+    "Finding joy in helping others. It brings purpose to my life.",
+    "I'm trapped in a never-ending cycle of pain. It's suffocating me.",
+    "Embracing self-care and self-love. Nurturing my well-being.",
+    "I'm consumed by thoughts of self-destruction. It's consuming me.",
+    "Seeking comfort in the beauty of art. It touches my soul.",
+    "I'm shattered. There's nothing left to hold on to.",
+    "Finding strength in vulnerability. Opening up to healing.",
+    "I'm on the verge of giving up. The world feels cold and dark.",
+]
+
+
+results = classify_texts(texts)
+print(results)
+
+
+
 
 
 
 
 # the code below this is to be integrated into the evaluation if necessary
 
-"""
+'''
 #function to put all collected tweet data into a dictionary and print it out
 def printtweetdata(n,ith_tweet, scrapped_data):
-
-    d = {"Tweet Scraped":n, "Username":ith_tweet[0], "Description":ith_tweet[1],"Tweet Text":ith_tweet[2], "Hashtags Used":ith_tweet[3],}
-
-    print(f"Tweet Scrapped{n}:")
-    print(f"Username:{ith_tweet[0]}")
-    print(f"Description:{ith_tweet[1]}")
-    print(f"Tweet Text:{ith_tweet[2]}")
-    print(f"Hashtags Used:{ith_tweet[3]}")
+    d = {"Tweet Scraped": n, "Username": ith_tweet[0], "Description": ith_tweet[1], "Tweet Text": ith_tweet[2],
+         "Hashtags Used": ith_tweet[3]}
     scrapped_data.append(d)
 
 
-#tweet scraping function
-def scrape(keywords, num_tweets, scrapped_data):
+#Tweet scraping function
+def scrape(keywords, num_tweets):
     print("runs")
-    tweets = tweepy.Cursor(api.search_tweets, q=keywords, lang="en",
-                           tweet_mode='extended').items(num_tweets)
+    tweets = tweepy.Cursor(api.search_tweets, q=keywords, lang="en", tweet_mode='extended').items(num_tweets)
 
     list_tweets = [tweet for tweet in tweets]
 
+    scrapped_data = []
     i = 1
 
-    #iteration
+    # Iteration
     for tweet in list_tweets:
         username = tweet.user.screen_name
         description = tweet.user.description
@@ -176,15 +292,18 @@ def scrape(keywords, num_tweets, scrapped_data):
         ith_tweet = [username, description, totaltweets, text, hashtext]
 
         printtweetdata(i, ith_tweet, scrapped_data)
-        i = i+1
+        i = i + 1
+
+    return scrapped_data
+
 
 #main function
 if __name__ == '__main__':
     # Set up your Twitter API credentials
-    consumer_key = "WC2RfdKkc3I3jvqW5Fk39ZSLJ"
-    consumer_secret = "perfdWp84L3tqcmCkWqKlP4jy3xXIrZtTLiWNgPESy9wPQp4ub"
-    access_token = "2459334069-dYcyulBBJC7YZOnlW8SihUWSdWl40SnSJxbdxD8"
-    access_token_secret = "HLFh68ySEJVNSjVoQEO7bsGBTBBEfcvxOsxX4Qmh53lOm"
+    consumer_key = "n0mE9SCjtQJUL7r99t4ef9hh4"
+    consumer_secret = "rAUbBXvj1vPp5vmbshqJGp3CLC8cumQi5cuNgLfnDeLiXkSjTO"
+    access_token = "1673053054931697664-NmJ61iIUmOqKupJ3wo1VKgjAaExRMo"
+    access_token_secret = "pYe1q7oZ6DxKasSfkM2KwoBuKgJmolKJJVolwCN8i9Ff5"
 
     # Authenticate with Twitter API
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -195,11 +314,22 @@ if __name__ == '__main__':
     keywords = "kill myself OR commit suicide OR self-harm"
 
     # Set the number of tweets you want to collect
-    num_tweets = 100
+    num_tweets = 40
 
     # Collect tweets based on keywords
-    scraped_data = []
     print("Fetching tweets...")
-    scrape(keywords, num_tweets, scraped_data)
+    scrapped_data = scrape(keywords, num_tweets)
     print("Scraping completed")
-"""
+
+    # Extract tweets and usernames from the collected data
+    texts = [data["Tweet Text"] for data in scrapped_data]
+    usernames = [data["Username"] for data in scrapped_data]
+
+    # Call the classify_texts function with the extracted texts
+    results = classify_texts(texts)
+
+    # Add the usernames to the results DataFrame
+    results['Username'] = usernames
+
+    print(results)
+'''
